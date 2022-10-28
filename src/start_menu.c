578b648f16875e1d322f9f75f4ae96eaeae366ce
orznick14 <orznick14@gmail.com>
2022-10-28 01:07:29 -0400

    Remove Saving Comfirmation
    https://github.com/pret/pokeemerald/wiki/Remove-the-extra-save-confirmation

--- src/start_menu.c
+++ src/start_menu.c
@@ -1010,7 +1010,7 @@ static u8 SaveConfirmSaveCallback(void)
     }
     else
     {
-        ShowSaveMessage(gText_ConfirmSave, SaveYesNoCallback);
+        sSaveDialogCallback = SaveSavingMessageCallback;
     }
 
     return SAVE_IN_PROGRESS;
